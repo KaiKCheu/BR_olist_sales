208cbd7b7785eae0c2dcfa667459632ab0a74f9d
Kai Cheu <kkjcheu@gmail.com>
2024-04-18 15:26:34 -0700

    updated route file
    additional query now finds the most popular routes per quarter

--- SQL_Files/popular_routes.sql
+++ SQL_Files/popular_routes.sql
@@ -33,7 +33,7 @@ SELECT * FROM br_ecommerce.olist_customers_dataset;
 SELECT COUNT(route) AS num_of_shipments, route, quarter
 FROM(
 	/* Seller state ships -> customer state. Create pairs of route data */
-	SELECT *, CONCAT(seller_state, ', ', customer_state) AS route
+	SELECT *, CONCAT(seller_city, ', ', customer_city) AS route
 	FROM(
 		SELECT oi.order_id, oi.seller_id, sd.seller_city, sd.seller_state, s.customer_id, c.customer_city, c.customer_state, s.quarter, s.order_status FROM br_ecommerce.olist_order_items_dataset oi
 		INNER JOIN (
@@ -69,5 +69,53 @@ GROUP BY e.route, e.quarter
 /* only look for shipments that have at occur at least 100 times a quarter */
 HAVING num_of_shipments > 100
 ORDER BY route DESC, num_of_shipments DESC;
+
+/* RANK the most popular routes per quarter */
+SELECT * 
+FROM (
+	SELECT *, DENSE_RANK() OVER(PARTITION BY quarter ORDER BY num_of_shipments DESC) AS 'rankings'
+	FROM (
+		SELECT COUNT(route) AS num_of_shipments, route, quarter
+		FROM(
+			/* Seller state ships -> customer state. Create pairs of route data */
+			SELECT *, CONCAT(seller_city, ', ', customer_city) AS route
+			FROM(
+				SELECT oi.order_id, oi.seller_id, sd.seller_city, sd.seller_state, s.customer_id, c.customer_city, c.customer_state, s.quarter, s.order_status FROM br_ecommerce.olist_order_items_dataset oi
+				INNER JOIN (
+					SELECT order_id, customer_id, order_status, CONCAT(YEAR(f.order_purchase_timestamp), ' ', f.quarter) AS quarter
+					FROM(
+						/* Divide the dataset into quarters */
+						SELECT *,
+						CASE
+							WHEN MONTH(order_purchase_timestamp) < 4 THEN 'Q1'
+							WHEN MONTH(order_purchase_timestamp) >= 4 AND MONTH(order_purchase_timestamp) < 7 THEN "Q2"
+							WHEN MONTH(order_purchase_timestamp) >= 7 AND MONTH(order_purchase_timestamp) < 10 THEN "Q3"
+							WHEN MONTH(order_purchase_timestamp) >= 10 THEN "Q4"
+						END AS quarter FROM br_ecommerce.olist_orders_dataset
+						/* Filter out shipments that are unavailable */
+						WHERE order_status NOT LIKE "unavailable"
+					) f
+					/* Filter out shipments that are canceled */
+					WHERE order_status NOT LIKE "canceled"
+				) s
+				ON s.order_id = oi.order_id
+				/* Join the customers data set to get the customer city, state.  Inner Join to remove any null seller city/state information */
+				INNER JOIN br_ecommerce.olist_customers_dataset c
+				ON s.customer_id = c.customer_id
+				/* Inner Join the sellers dataset to get the seller city, state.  Inner join to remove any null seller city/state information */
+				INNER JOIN br_ecommerce.olist_sellers_dataset sd
+				ON oi.seller_id = sd.seller_id
+				/* Filter out any shipments that are processing */
+				WHERE s.order_status NOT LIKE 'processing'
+			) t
+		) e
+		/* Group by route and quarter to see the most popular routes for each quarter */
+		GROUP BY e.route, e.quarter
+		/* only look for shipments that have at occur at least 100 times a quarter */
+		HAVING num_of_shipments > 20
+		ORDER BY route DESC, num_of_shipments DESC
+	) a
+) b
+WHERE rankings <= 10;
 
 
